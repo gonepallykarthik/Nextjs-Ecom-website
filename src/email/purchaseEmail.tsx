4d6dde72bec90cbaa2ca735cf4fd8f8729a53b40
AdithyaKumarG <adithyakumar049@gmail.com>
2024-06-06 14:29:21 +0530

    updated puchasePage

--- src/email/purchaseEmail.tsx
+++ src/email/purchaseEmail.tsx
@@ -19,21 +19,6 @@ type PurchaseReceiptEmailProps = {
   downloadVerificationId: string;
 };
 
-PurchaseReceiptEmail.PreviewProps = {
-  product: {
-    name: "Product name",
-    description: "Some description",
-    imagePath:
-      "/products/5aba7442-e4a5-4d2e-bfa7-5bd358cdad64-02 - What Is Next.js.jpg",
-  },
-  order: {
-    id: crypto.randomUUID(),
-    createdAt: new Date(),
-    price: 10000,
-  },
-  downloadVerificationId: crypto.randomUUID(),
-} satisfies PurchaseReceiptEmailProps;
-
 export default function PurchaseReceiptEmail({
   product,
   order,
